bd29ef1621f9eb493d1c5922a16b03d1aa5d5158
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-01-09 15:30:36 +0500

    Setup Backend Project and install some dependencies and run server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+  res.send("Authentication Backend!");
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
